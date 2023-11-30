92c10cf702661e988c5a4cead8308e1043f3d0b4
JonathanValle12 <jonathin_2002@outlook.es>
2023-11-30 16:35:40 +0100

    MENU AGREGADO

--- app/src/main/java/com/example/movierecommender/MainActivity.kt
+++ app/src/main/java/com/example/movierecommender/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.movierecommender
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
@@ -8,5 +12,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_peliculas_todas)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.accion_favoritos -> {
+                Toast.makeText(this, "Hola", Toast.LENGTH_LONG).show();
+                //setContentView(R.layout.activity_peliculas_favoritas)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
