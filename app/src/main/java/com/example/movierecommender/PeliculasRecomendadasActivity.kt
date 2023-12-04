f6913558f650d66f86f9512e4696d618a639fb54
Jonathan12V <146198646+Jonathan12V@users.noreply.github.com>
2023-12-04 16:21:13 +0100

    Jonathan (#12)
    * Layout de peliculas recomendadas y todas en fase
    
    * Implementación layout de informacion, iconos
    
    * Layouts de Peliculas e implementacón del header
    
    * Implementación algunos layouts mas + menu y archivos generales
    
    * Archivos layouts implementados + temas, strings..
    
    * Vista Perfil y carpeta values modificación
    
    * MENU AGREGADO
    
    * Implementar menu de navegación y adaptación del codigo
    
    * Arreglo RadioButton de las peliculas
    
    ---------
    
    Co-authored-by: JonathanValle12 <jonathin_2002@outlook.es>

--- app/src/main/java/com/example/movierecommender/PeliculasRecomendadasActivity.kt
+++ app/src/main/java/com/example/movierecommender/PeliculasRecomendadasActivity.kt
@@ -1,6 +1,7 @@
 package com.example.movierecommender
 
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class PeliculasRecomendadasActivity : MenuActivity() {
@@ -9,5 +10,16 @@ class PeliculasRecomendadasActivity : MenuActivity() {
 
         val settingsContent = layoutInflater.inflate(R.layout.activity_peliculas_recomendadas, null)
         findViewById<ConstraintLayout>(R.id.fragment_container).addView(settingsContent)
+
+        val buttonTodo: RadioButton = findViewById(R.id.Todo);
+        val buttonRecomendadas: RadioButton = findViewById(R.id.recomendados);
+        buttonTodo.setOnClickListener {
+            buttonTodo.isChecked = true;
+            buttonRecomendadas.isChecked = false;
+        }
+        buttonRecomendadas.setOnClickListener {
+            buttonRecomendadas.isChecked = true;
+            buttonTodo.isChecked = false;
+        }
     }
 }
